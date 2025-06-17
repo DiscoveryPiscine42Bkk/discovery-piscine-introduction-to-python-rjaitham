3aa1cca828b8ab5e1a2477c90600b2223da0393e
rjaitham <rapee.mynum@gmail.com>
2025-06-17 07:06:01 +0000

    create_array.py

---
+++ cell05/ex5.0/create_array.py
@@ -0,0 +1,10 @@
+def main():
+    # Define the array of numbers
+    numbers = [2, 8, 9, 48, 8, 22, -12, 2]
+    
+    # Display the array
+    print(numbers)
+
+if __name__ == "__main__":
+    main()
+    
