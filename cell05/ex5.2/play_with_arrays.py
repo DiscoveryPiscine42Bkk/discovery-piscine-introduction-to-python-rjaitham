36d25270e2d271f0cc7710ce8b0196c716bc7094
rjaitham <rapee.mynum@gmail.com>
2025-06-18 03:25:34 +0000

    play_with_arrays.py

--- cell05/ex5.2/play_with_arrays.py
+++ cell05/ex5.2/play_with_arrays.py
@@ -1,7 +1,13 @@
+# play_with_arrays.py
+
+# Original array
 arr = [2, 8, 9, 48, 8, 22, -12, 2]
 
+# Filter values greater than 5
 filtered_arr = [x for x in arr if x > 5]
 
+# Process the filtered array (for example, multiplying each value by 2)
 processed_arr = [x * 2 for x in filtered_arr]
 
+# Print the result (similar to the example output)
 print(processed_arr)
