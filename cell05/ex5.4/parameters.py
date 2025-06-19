ad2fad471d552c6813d604ead0933c542be9e55c
rjaitham <rapee.mynum@gmail.com>
2025-06-19 03:57:25 +0000

    parameters.py

--- cell05/ex5.4/parameters.py
+++ cell05/ex5.4/parameters.py
@@ -1,8 +1,5 @@
-# parameters.py
 import sys
 
-# Calculate the number of parameters (excluding the script name)
 num_params = len(sys.argv) - 1
 
-# Display the number of parameters
 print(f"Number of parameters: {num_params}.")
